0a3274c38a6deffb0c082669619f7a9f4265101b
anakettl <anakettl@gmail.com>
2021-03-08 20:52:11 -0300

    first commit

---
+++ src/main/java/edu/ifrs/vvs/App.java
@@ -0,0 +1,47 @@
+/**
+ * @License
+ * Copyright 2020 Bubble Sort Application
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.ifrs.vvs;
+
+/**
+ * Bubble Sort App.
+ */
+public final class App {
+
+    private App() {
+
+    }
+
+    /**
+     * Sorts the array.
+     *
+     * @param args The arguments of the program
+     */
+    // Driver code
+    public static void main(String args[])
+    {
+        //int arr[] = { 120, 11, 13, 5, 6, 7 };
+        HeapSort hs = new HeapSort();
+        int[] arr = hs.read();
+
+        HeapSort ob = new HeapSort();
+        hs.sort(arr);
+
+        System.out.println("Sorted array is");
+        ob.printArray(arr);
+    }
+}
