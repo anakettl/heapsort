0d2229517e0b0ad384054e0257058aaff58b9159
anakettl <anakettl@gmail.com>
2021-03-08 21:56:04 -0300

    adjust braces

--- src/main/java/edu/ifrs/vvs/HeapSort.java
+++ src/main/java/edu/ifrs/vvs/HeapSort.java
@@ -31,12 +31,14 @@ public class HeapSort {
      *
      * https://www.geeksforgeeks.org/heap-sort/
      */
-    public void sort(int arr[]) {
+    public void sort(int[] arr) {
         int n = arr.length;
 
         // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
+        }
+
 
         // One by one extract an element from heap
         for (int i = n - 1; i > 0; i--) {
@@ -53,18 +55,20 @@ public class HeapSort {
     /**
      * To heapify a subtree rooted with node i which is an index in arr[]. n is size of heap
      */
-    void heapify(int arr[], int n, int i) {
+    void heapify(int[] arr, int n, int i) {
         int largest = i; // Initialize largest as root
         int l = 2 * i + 1; // left = 2*i + 1
         int r = 2 * i + 2; // right = 2*i + 2
 
         // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
+        if (l < n && arr[l] > arr[largest]) {
             largest = l;
+        }
 
         // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
+        if (r < n && arr[r] > arr[largest]) {
             largest = r;
+        }
 
         // If largest is not root
         if (largest != i) {
@@ -107,7 +111,7 @@ public class HeapSort {
      * A utility function to print array of size n
      * @param arr the array
      */
-    static void printArray(int arr[]) {
+    static void printArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
