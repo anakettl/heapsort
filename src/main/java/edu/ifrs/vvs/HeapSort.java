1b632a8134db25d8f2eaa0856c3fe47d99ff387b
anakettl <anakettl@gmail.com>
2021-03-09 19:42:12 -0300

    add spaces

--- src/main/java/edu/ifrs/vvs/HeapSort.java
+++ src/main/java/edu/ifrs/vvs/HeapSort.java
@@ -55,7 +55,7 @@ public class HeapSort {
     /**
      * To heapify a subtree rooted with node i which is an index in arr[]. n is size of heap
      */
-    @SuppressWarnings({"jacoco.complexity","jacoco.method","jacoco.branch"})
+    @SuppressWarnings({"jacoco.complexity", "jacoco.method", "jacoco.branch"})
     void heapify(int[] arr, int n, int i) {
         int largest = i; // Initialize largest as root
         int l = 2 * i + 1; // left = 2*i + 1
