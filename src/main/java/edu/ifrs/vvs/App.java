4f517bb76124aa83c39438d4fd889d193859b9cc
anakettl <anakettl@gmail.com>
2021-03-08 21:36:19 -0300

    adjust brackets

--- src/main/java/edu/ifrs/vvs/App.java
+++ src/main/java/edu/ifrs/vvs/App.java
@@ -32,9 +32,7 @@ public final class App {
      * @param args The arguments of the program
      */
     // Driver code
-    public static void main(String args[])
-    {
-        //int arr[] = { 120, 11, 13, 5, 6, 7 };
+    public static void main(String[] args) {
         HeapSort hs = new HeapSort();
         int[] arr = hs.read();
 
